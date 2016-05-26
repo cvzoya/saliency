86b9b8d63875c35da0f139984e748b3b9e03e42b
dberga <maildber@gmail.com>
2016-05-26 18:47:17 +0200

    added IG metric

---
+++ code_forMetrics/IG.m
@@ -0,0 +1,31 @@
+% created: David Berga, May 2016
+
+% This finds the Information Gain between a saliency map and the ground
+% truth fixation map.
+% It is a non-symmetric measure of the information gain when saliencyMap 
+% is used to estimate fixationMap by substracting a baseline map, 
+% which is the averaged the ground truth fixation maps of all other images
+
+
+function [score,map] = IG(saliencyMap, fixationMap, baselineMap)
+% saliencyMap is the saliency map
+% fixationMap is the binary human fixation map
+% baselineMap is the saliency mean from other human fixation maps
+
+map1 = im2double(imresize(saliencyMap, size(fixationMap)));
+map2 = im2double(fixationMap);
+map3 = im2double(imresize(baselineMap, size(fixationMap)));
+
+% make sure map1 and map3 sum to 1
+if any(map1(:))
+    map1 = map1/sum(map1(:));
+end
+
+if any(map3(:))
+    map3 = map3/sum(map3(:));
+end
+
+% compute IG
+score = mean(mean(map2 .* (log2(eps + map1) - log2(eps+map3))));
+map = map2 .* (log2(eps + map1) - log2(eps+map3));
+
